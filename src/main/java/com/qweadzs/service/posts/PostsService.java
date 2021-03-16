c5f62fbe290b8d0fe493dc790459f23ff29011e3
박현우 <qweadzs@naver.com>
2021-03-17 02:44:00 +0900

    머스테치 의존성 추가. header, footer 머스테치 레이아웃식 구성 및 bootstrap CDN 적용. 게시글 등록, 수정, 삭제 머스테치, API, JS 추가.

--- src/main/java/com/qweadzs/service/posts/PostsService.java
+++ src/main/java/com/qweadzs/service/posts/PostsService.java
@@ -2,12 +2,20 @@ package com.qweadzs.service.posts;
 
 import com.qweadzs.domain.posts.Posts;
 import com.qweadzs.domain.posts.PostsRepository;
+import com.qweadzs.web.dto.PostsListResponseDto;
 import com.qweadzs.web.dto.PostsResponseDto;
 import com.qweadzs.web.dto.PostsSaveRequestDto;
 import com.qweadzs.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -21,17 +29,34 @@ public class PostsService {
 
     @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
-        Posts posts = postsRepository.findById(id).orElseThrow(() ->
-                new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
     }
 
-    public PostsResponseDto findById(Long id){
-        Posts entity = postsRepository.findById(id).orElseThrow(() ->
-                new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
+    @Transactional(readOnly = true)
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
         return new PostsResponseDto(entity);
     }
 
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
 }
--- src/test/java/com/qweadzs/g.java
+++
@@ -1,4 +0,0 @@
-package com.qweadzs;
-
-public class g {
-}
