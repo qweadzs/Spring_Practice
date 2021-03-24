191413490079af2125a16e51ebadf83267d90a77
박현우 <qweadzs@naver.com>
2021-03-25 00:11:23 +0900

    naver 로그인 의존성, 시크릿 키, 클라이언트ID 등록

--- src/main/java/com/qweadzs/web/IndexController.java
+++ src/main/java/com/qweadzs/web/IndexController.java
@@ -1,6 +1,7 @@
 package com.qweadzs.web;
 
 
+import com.qweadzs.config.auth.LoginUser;
 import com.qweadzs.config.auth.dto.SessionUser;
 import com.qweadzs.service.posts.PostsService;
 import com.qweadzs.web.dto.PostsResponseDto;
@@ -19,9 +20,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
